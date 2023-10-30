3c1d96238ec1dedc58c39d15900b5fca699fd252
LukeTheLuquefier <erickson.luked@gmail.com>
2023-10-29 20:03:17 -0700

    Updated main menu

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Reflection.Metadata.Ecma335;
+﻿using System.ComponentModel;
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 using static System.Console;
 namespace Tier_tac_toe;
 
@@ -7,7 +9,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        char?[,] spaces = new char?[9,2];
+        char?[,] spaces = new char?[9, 2];
+        string[,] AIMenu =
+        {
+            {
+                "Select AI mode:", "0 - Off", "1 - Easy", "2 - Medium", "3 - Hard"
+            },
+            {
+                "0","0","1", "2","3"
+            }
+        };
+        char AIMode = '0';
+        string[,] ruleMenu =
+        {
+            {
+                "Rules, select to change:", "1 - Allows you to places numbers on your own smaller numbers", "r - Resets all rules"
+            },
+            {
+                "","1","r"
+            }
+        };
+        bool[] rules =
+        {
+            false, false
+        };
+
+
         void ResetBoard(char gameMode1)
         {
             if (gameMode1 == '1' || gameMode1 == '2')
@@ -73,9 +100,9 @@ a : To enable AI
         }
 
         char currentPlayer11 = '0';
-        int CheckMove(int inputspace, char inputnumber, char currentPlayer11)
+        int CheckMove(int selectedSpace1, char selectedNumber1, char currentPlayer11, out int otherCodes)
         {
-
+            otherCodes = 1;
             return 0;
         }
 
@@ -92,7 +119,7 @@ a : To enable AI
                 WriteLine("");
                 Write("Select number: ");
                 selectedNumber = ReadKey().KeyChar;
-                correctInput = CheckMove(selectedSpace, selectedNumber, currentPlayer11);
+                correctInput = CheckMove(selectedSpace, selectedNumber, currentPlayer1, out int otherCodes);
                     if (correctInput != 0)
                 {
                     Clear();
@@ -103,35 +130,86 @@ a : To enable AI
         }
 
 
-        bool CheckWinCons()
+        int CheckWinCons()
         {
-            return true;
+            return 9;
         }
+
         void WinChanges()
         {
 
         }
-        char GameModeSelect()
+
+        void MenuSelect(ref string[,] theMenu)
+        {
+
+            while (true)
+            {
+                Clear();
+                for (int number03 = 0; number03 < theMenu.GetLength(1); number03++)
+                {
+                    WriteLine(theMenu[0, number03]);
+                }
+                char menuInput = ReadKey().KeyChar;
+                if (menuInput == 'b')
+                {
+                    theMenu[1, 0] = "b";
+                    return;
+                }
+                for (int number13 = 1; number13 < theMenu.GetLength(1); number13++)
+                {
+                    if (menuInput == Convert.ToChar(theMenu[1, number13]))
+                    {
+                        theMenu[1, 0] = (theMenu[1, number13]);
+                        return;
+                    }
+                }
+                WriteLine("Not a valid input");
+            }
+
+        }
+        void ResetRules()
+        {
+            rules = new bool[]{false, false};
+        }
+        char MainMenuSelect()
         {
             while (true)
             {
                 Clear();
-                WriteLine("Mode select:\n1 - tic tac toe\n2 - tier tac toe\ne - exit game");
+                WriteLine("Mode select:\n1 - tic tac toe\n2 - tier tac toe\nr - rules\na - AI menu\ne - exit game");
                 switch (ReadKey().KeyChar)
                 {
                     case '1':
                         return '1';
                     case '2':
                         return '2';
+                    case 'r':
+                        MenuSelect(ref ruleMenu);
+                        if (ruleMenu[1, 0] == "b")
+                            continue;
+                        if (ruleMenu[1, 0] == "r")
+                        {
+                            ResetRules();
+                            continue;
+                        }
+                        rules[Convert.ToInt32(ruleMenu[1, 0])] = (rules[Convert.ToInt32(ruleMenu[1, 0])] ? false : true);
+                        continue;
+                    case 'a':
+                        MenuSelect(ref AIMenu);
+                        if (AIMenu[1, 0] == "b")
+                            continue;
+                        AIMode = Convert.ToChar(AIMenu[1, 0]);
+                        continue;
                     case 'e':
                         return 'e';
                 }
-                Clear();
             }
+            return '0';
         }
 
 
-        char gameMode = GameModeSelect();
+        char gameMode = MainMenuSelect();
         int win;
         int condition = 0;
         char currentPlayer = '0';
@@ -153,7 +231,7 @@ a : To enable AI
                 }
             }
             Thread.Sleep(4000);
-            gameMode = GameModeSelect();
+            gameMode = MainMenuSelect();
         }
         
 
